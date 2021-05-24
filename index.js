baf725ffa6ca74ea0912c5f2f22def2f553344e6
niceRAM <niceniceram@gmail.com>
2021-05-24 17:51:27 +0800

    格式化一下，方便之后查看差分

--- index.js
+++ index.js
@@ -6,74 +6,76 @@ const html404 = `<!DOCTYPE html>
 
 
 async function randomString(len) {
-　　len = len || 6;
-　　let $chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';    /****默认去掉了容易混淆的字符oOLl,9gq,Vv,Uu,I1****/
-　　let maxPos = $chars.length;
-　　let result = '';
-　　for (i = 0; i < len; i++) {
-　　　　result += $chars.charAt(Math.floor(Math.random() * maxPos));
-　　}
-　　return result;
+  len = len || 6;
+  let $chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';    /****默认去掉了容易混淆的字符oOLl,9gq,Vv,Uu,I1****/
+  let maxPos = $chars.length;
+  let result = '';
+  for (i = 0; i < len; i++) {
+    result += $chars.charAt(Math.floor(Math.random() * maxPos));
+  }
+  return result;
 }
-async function checkURL(URL){
-    let str=URL;
-    let Expression=/http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- .\/?%&=]*)?/;
-    let objExp=new RegExp(Expression);
-    if(objExp.test(str)==true){
-      if (str[0] == 'h')
-        return true;
-      else
-        return false;
-    }else{
-        return false;
-    }
-} 
-async function save_url(URL){
-    let random_key=await randomString()
-    let is_exist=await LINKS.get(random_key)
-    console.log(is_exist)
-    if (is_exist == null)
-        return await LINKS.put(random_key, URL),random_key
+async function checkURL(URL) {
+  let str = URL;
+  let Expression = /http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- .\/?%&=]*)?/;
+  let objExp = new RegExp(Expression);
+  if (objExp.test(str) == true) {
+    if (str[0] == 'h')
+      return true;
     else
-        save_url(URL)
+      return false;
+  } else {
+    return false;
+  }
+}
+async function save_url(URL) {
+  let random_key = await randomString()
+  let is_exist = await LINKS.get(random_key)
+  console.log(is_exist)
+  if (is_exist == null)
+    return await LINKS.put(random_key, URL), random_key
+  else
+    save_url(URL)
 }
 async function handleRequest(request) {
   console.log(request)
   if (request.method === "POST") {
-    let req=await request.json()
+    let req = await request.json()
     console.log(req["url"])
-    if(!await checkURL(req["url"])){
-    return new Response(`{"status":500,"key":": Error: Url illegal."}`, {
-      headers: {
-      "content-type": "text/html;charset=UTF-8",
-      "Access-Control-Allow-Origin":"*",
-      "Access-Control-Allow-Methods": "POST",
-      },
-    })}
-    let stat,random_key=await save_url(req["url"])
+    if (!await checkURL(req["url"])) {
+      return new Response(`{"status":500,"key":": Error: Url illegal."}`, {
+        headers: {
+          "content-type": "text/html;charset=UTF-8",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "POST",
+        },
+      })
+    }
+    let stat, random_key = await save_url(req["url"])
     console.log(stat)
-    if (typeof(stat) == "undefined"){
-      return new Response(`{"status":200,"key":"/`+random_key+`"}`, {
-      headers: {
-      "content-type": "text/html;charset=UTF-8",
-      "Access-Control-Allow-Origin":"*",
-      "Access-Control-Allow-Methods": "POST",
-      },
-    })
-    }else{
+    if (typeof (stat) == "undefined") {
+      return new Response(`{"status":200,"key":"/` + random_key + `"}`, {
+        headers: {
+          "content-type": "text/html;charset=UTF-8",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "POST",
+        },
+      })
+    } else {
       return new Response(`{"status":200,"key":": Error:Reach the KV write limitation."}`, {
+        headers: {
+          "content-type": "text/html;charset=UTF-8",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "POST",
+        },
+      })
+    }
+  } else if (request.method === "OPTIONS") {
+    return new Response(``, {
       headers: {
-      "content-type": "text/html;charset=UTF-8",
-      "Access-Control-Allow-Origin":"*",
-      "Access-Control-Allow-Methods": "POST",
-      },
-    })}
-  }else if(request.method === "OPTIONS"){  
-      return new Response(``, {
-      headers: {
-      "content-type": "text/html;charset=UTF-8",
-      "Access-Control-Allow-Origin":"*",
-      "Access-Control-Allow-Methods": "POST",
+        "content-type": "text/html;charset=UTF-8",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "POST",
       },
     })
 
@@ -82,24 +84,24 @@ async function handleRequest(request) {
   const requestURL = new URL(request.url)
   const path = requestURL.pathname.split("/")[1]
   console.log(path)
-  if(!path){
+  if (!path) {
 
-    const html= await fetch("https://cdn.jsdelivr.net/gh/xyTom/Url-Shorten-Worker@gh-pages/index.html")
-    
+    const html = await fetch("https://cdn.jsdelivr.net/gh/xyTom/Url-Shorten-Worker@gh-pages/index.html")
+
     return new Response(await html.text(), {
-    headers: {
-      "content-type": "text/html;charset=UTF-8",
-    },
-  })
+      headers: {
+        "content-type": "text/html;charset=UTF-8",
+      },
+    })
   }
   const value = await LINKS.get(path)
   console.log(value)
-  
+
 
   const location = value
   if (location) {
     return Response.redirect(location, 302)
-    
+
   }
   // If request not in kv, return 404
   return new Response(html404, {
